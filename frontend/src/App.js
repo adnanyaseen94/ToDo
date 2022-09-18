fbd553db152406069ae31e5a46abf098c72e1b41
adnanyaseen94 <adnanyaseen94@hotmail.com>
2022-09-18 21:47:17 +0100

    App.js: handle login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,61 @@
-
+import { useEffect, useState } from 'react'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
 
 function App() {
+  const [userLogged, setUserLogged] = useState(false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('userToken')
+    if (token) setUserLogged(true)
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const token = await loginService.login({
+        username, password
+      })
+      setUserLogged(true)
+      window.localStorage.setItem(
+        'userToken', token.token
+      )
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+      console.log(exception)
+    }
+  }
+
+  const handleLogout = () => {
+    setUserLogged(false)
+    window.localStorage.removeItem('userToken')
+  }
+
   return (
-    <div>
-      To do list
-    </div>
+    <>
+      <h2>
+        To do list
+      </h2>
+      <div>
+        { userLogged ?
+          <div>
+            welcome
+            <button onClick={ handleLogout }>logout</button>
+          </div> :
+          <LoginForm
+            username={ username }
+            password={ password }
+            handleUsernameChange={({ target }) => setUsername(target.value) }
+            handlePasswordChange={({ target }) => setPassword(target.value) }
+            handleSubmit={ handleLogin }
+          />
+        }
+      </div>
+    </>
   );
 }
 
